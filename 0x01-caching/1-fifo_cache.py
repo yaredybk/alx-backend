80707911a408b1711e67cd5e4c3be36d69710f9d
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2024-08-22 03:13:08 +0300

    task-3

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Caching
+Caching.
 
 FIFO Cache
 """
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
 """
-Caching
+Caching.
+
+LRU Cache
 """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU Caching."""
+
+    def __init__(self):
+        """Initialize."""
+        super().__init__()
+
+    def put(self, key, item):
+        """Put item value for the key key."""
+        if key is None or item is None:
+            return
+        if self.cache_data.get(key) is not None:
+            del self.cache_data[key]
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            for k in self.cache_data.keys():
+                self.cache_data.pop(k)
+                print(f'DISCARD: {k}')
+                break
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item from cache."""
+        if key is None:
+            return None
+        item = self.cache_data.get(key)
+        if item is None:
+            return None
+        del self.cache_data[key]
+        self.cache_data[key] = item
+        return item
