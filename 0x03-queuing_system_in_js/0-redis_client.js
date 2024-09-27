3b68c0ddee3c594e3277b36dd45a5e011f286c18
yared b <yb12ybk@gmail.com>
2024-09-27 19:31:53 +0000

    init

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * Redis Basic
+ */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
---
+++ 0x03-queuing_system_in_js/README.md
@@ -0,0 +1,9 @@
+0x03. Queuing System in JS
+Back-end
+JavaScript
+ES6
+Redis
+NodeJS
+ExpressJS
+Kue
+ Weight: 1
